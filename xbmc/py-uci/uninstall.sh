546eee060f928da952827898c2b83d877f7851cb
macrosea <macrosea@gmail.com>
2015-01-10 15:55:03 +0800

    add ..

---
+++ xbmc/py-uci/uninstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -f /usr/share/dbus-1/system-services/com.livecloud.uci.service
+rm -f /etc/dbus-1/system.d/com.livecloud.uci.conf
+
+rm -f /usr/local/sbin/uci-*
+rm -f /usr/local/bin/uci-*
